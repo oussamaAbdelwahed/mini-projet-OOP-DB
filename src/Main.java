408cf1dc2888cd8be18646a083bd4f9323b731d1
oussamaAbdelwahed <ousabdelwahed@gmail.com>
2020-03-21 14:54:53 +0100

    set up javafx

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
 
 
-public class Main {
-   public static void main(String [] args) {
-	   System.out.println("benne");
-   }
- }
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Button btn = new Button("TEXT BTN");
+        
+        Scene sc = new Scene(btn,400,400);
+        primaryStage.setScene(sc);
+        
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
